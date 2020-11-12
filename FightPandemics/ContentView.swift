5e409b2872af2038fd9f668300ad04b651a31b5a
Benjamin Rettig <benrettig@icloud.com>
2020-11-12 11:13:25 -0700

    in-app registration and log-in views + some tabbar +toolbar ui

--- FightPandemics/ContentView.swift
+++ FightPandemics/ContentView.swift
@@ -2,59 +2,23 @@
 //  ContentView.swift
 //  FightPandemics
 //
-//  Created by Scott on 9/1/20.
-//  Copyright © 2020 Scott. All rights reserved.
-//
 
 import SwiftUI
 
 struct ContentView: View {
-    
-    var body: some View {
-        
-        NavigationView {
-            
-            TabView {
+    init() {UINavigationBar
+        .appearance()
+        .titleTextAttributes = [.font: UIFont(name: "Verdana", size: 16)!]   }
 
-                Home()
-                    .tabItem {
-                        Image("home-icon")
-                        Text("Home")
-                }
-                
-                Search()
-                    .tabItem {
-                        Image("magnifier-icon")
-                        Text("Search")
-                }
-                
-                Post()
-                    .tabItem {
-                        Image(systemName: "plus.circle")
-                        Text("Post")
-                }
-                
-                Inbox()
-                    .tabItem {
-                        Image("inbox-icon")
-                        Text("Inbox")
-                }
-                
-                Profile()
-                    .tabItem {
-                        Image("profile-icon")
-                        Text("Profile")
-                }
-            }
-            .navigationBarItems(leading: Image("nav-logo").padding(-5), trailing: Image(systemName: "location"))
-            .font(.title)
-        
+    var body: some View {
+   
+ VStack {
+
+        TabControllerView()
         }
-    }
-}
+        .accentColor(Color( #colorLiteral(red: 0.2590000033, green: 0.3529999852, blue: 0.949000001, alpha: 1)))   }}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-    }
-}
+        ContentView()  }}
